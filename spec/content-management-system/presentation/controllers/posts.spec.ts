e8236eab6591a150d36f176aa7cfaa1896d8cf29
CAPCJ <capcj@protonmail.com>
2024-05-26 20:56:26 -0300

    test(posts): WIP add failing tests related to posts endpoint returning the user posts

--- spec/content-management-system/presentation/controllers/posts.spec.ts
+++ spec/content-management-system/presentation/controllers/posts.spec.ts
@@ -5,9 +5,29 @@ import { HttpStatusCode } from "@src/utils/httpStatusCode";
 describe("[CMS] Posts", () => {
   describe("GET /cms/posts", () => {
     it("responds with all user posts", async () => {
+      const expected = {
+        posts: [
+          {
+            id: 1,
+            category: "Nerdy stuff",
+            content: "Testing some nerdy stuff",
+            createdAt: expect.any(String),
+            updatedAt: expect.any(String),
+          },
+          {
+            id: 2,
+            category: "Career",
+            content: "A serious blog post regarding career",
+            createdAt: expect.any(String),
+            updatedAt: expect.any(String),
+          },
+        ],
+      };
+
       const response = await request(app).get("/cms/posts");
       expect(response.statusCode).toBe(HttpStatusCode.OK);
       expect(response.body).toBeInstanceOf(Object);
+      expect(response.body).toMatchObject(expected);
     });
   });
 });
