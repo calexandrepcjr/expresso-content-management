8502519087661993501cc74e7106ebc17c9993cd
CAPCJ <capcj@protonmail.com>
2024-05-29 14:44:58 -0300

    refactor: add home guiding to api-docs

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,25 @@
 import { routes as cmsRoutes } from "@src/content-management-system/presentation/routes";
+import { defaultEndpointsFactory } from "express-zod-api";
+import { z } from "zod";
 
 export const routes = {
+  "": defaultEndpointsFactory.build({
+    method: "get",
+    shortDescription: "API home",
+    input: z.object({}),
+    output: z.object({
+      greetings: z.string(),
+    }),
+    handler: async ({ input, options, logger }) => {
+      logger.debug("Received:", {
+        options,
+        input,
+      });
+
+      return {
+        greetings: "Check the routes in the Swagger UI, routing to /api-docs",
+      };
+    },
+  }),
   ...cmsRoutes,
 };
