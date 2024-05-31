6eb5cd6871a02281db2cfbadd74278d8fa89b6cf
CAPCJ <capcj@protonmail.com>
2024-05-30 21:20:27 -0300

    refactor(posts): replace relative paths

--- src/content-management-system/presentation/routes.ts
+++ src/content-management-system/presentation/routes.ts
@@ -1,11 +1,11 @@
 import { Routing, DependsOnMethod } from "express-zod-api";
-import { createPost } from "./controllers/createPost";
-import { getHealth } from "./controllers/getHealth";
-import { getRoot } from "./controllers/getRoot";
-import { getPosts } from "./controllers/getPosts";
-import { getPostById } from "./controllers/getPostById";
-import { updateWholePost } from "./controllers/updateWholePost";
-import { removeAllUserPosts } from "./controllers/removeAllUserPosts";
+import { createPost } from "@src/content-management-system/presentation/controllers/createPost";
+import { getHealth } from "@src/content-management-system/presentation/controllers/getHealth";
+import { getRoot } from "@src/content-management-system/presentation/controllers/getRoot";
+import { getPosts } from "@src/content-management-system/presentation/controllers/getPosts";
+import { getPostById } from "@src/content-management-system/presentation/controllers/getPostById";
+import { updateWholePost } from "@src/content-management-system/presentation/controllers/updateWholePost";
+import { removeAllUserPosts } from "@src/content-management-system/presentation/controllers/removeAllUserPosts";
 
 export const routes: Routing = {
   cms: {
