69e1d29c117e9d6566f8a0ec4286f2bfc2c3e672
CAPCJ <capcj@protonmail.com>
2024-05-30 23:00:00 -0300

    test(auth): add create user spec

---
+++ spec/auth/presentation/controllers/createUser.spec.ts
@@ -0,0 +1,35 @@
+import request from "supertest";
+import { HttpStatusCode } from "@src/utils/httpStatusCode";
+import { faker } from "@faker-js/faker";
+import { InternetMediaType } from "@src/utils/internetMediaType";
+
+const localRequest = request("http://localhost:3000");
+
+describe("[CMS] Posts", () => {
+  describe("POST /auth/users", () => {
+    it("creates a user", async () => {
+      const expected = {
+        status: "created",
+        data: {
+          createdAt: expect.any(String),
+          token: expect.any(String),
+        },
+      };
+      const payload = {
+        fullName: faker.person.fullName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+      };
+
+      const response = await localRequest
+        .post("/auth/users")
+        .send(payload)
+        .set("Content-Type", InternetMediaType.ApplicationJson)
+        .set("Accept", InternetMediaType.ApplicationJson);
+
+      expect(response.statusCode).toBe(HttpStatusCode.Created);
+      expect(response.body).toBeInstanceOf(Object);
+      expect(response.body).toMatchObject(expected);
+    });
+  });
+});
